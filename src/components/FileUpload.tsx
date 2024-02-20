5d4fcb01b76ca5879caeb025a48f8ab0a04268dc
gabriels5g <gabrielspxls@gmail.com>
2024-02-20 13:07:39 -0300

    creating the close image button

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { UploadDropzone } from '@/lib/uploadthing'
 import '@uploadthing/react/styles.css'
+import { X } from 'lucide-react'
 import Image from 'next/image'
 interface FileUploadProps {
   onChange: (url?: string) => void
@@ -15,6 +16,13 @@ export function FileUpload({ endpoint, onChange, value }: FileUploadProps) {
     return (
       <div className="relative h-20 w-20">
         <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
       </div>
     )
   }
